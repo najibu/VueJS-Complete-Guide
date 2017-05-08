56d6164cd1bdbb919fd5e847c28ce16c840f6380
Najibu Nsubuga <najibu@brandmovers.co.uk>
2017-05-08 13:35:32 +0100

    Finished ex 4

--- 1_dom_interaction/Exercise 4/app.js
+++ 1_dom_interaction/Exercise 4/app.js
@@ -4,18 +4,36 @@ new Vue({
     effectClasses: {
       highlight: false,
       shrink: true
+    },
+    float: 'float',
+    userClass: '',
+    isVisible: true,
+    myStyle: {
+      backgroundColor: 'gray',
+      height: '150px',
+      width: '100px'
+    },
+    progressBar: {
+      width: '0px',
+      backgroundColor: 'red'
     }
   },
   methods: {
     startEffect: function() {
       var vm = this;
-      setInterval(function(){
-        vm.effectClasses.highlight = !vm.effectClasses.highlight;
-        vm.effectClasses.highlight = !vm.effectClasses.highlight;
+      setInterval(function() {
+      vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
       }, 1000);
     },
     startProgress: function(){
+      var vm = this;
+      var width = 0;
 
+      setInterval(function(){
+        width += 10;
+        vm.progressBar.width = width + 'px';
+      }, 500);
     }
   }
 });
