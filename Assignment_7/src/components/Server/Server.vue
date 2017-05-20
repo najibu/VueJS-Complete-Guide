4013adb8e23a2578c7e6c407ec26e7e5fba766fb
najibu <ns.najibu@gmail.com>
2017-05-20 12:33:04 +0100

    Completed assignment

---
+++ Assignment_7/src/components/Server/Server.vue
@@ -0,0 +1,27 @@
+<template>
+  <li
+    class="list-group-item"
+    style="cursor: pointer"
+    @click="serverSelected"
+  >
+    Server #{{ server.id }}
+  </li>
+</div>
+</template>
+
+<script>
+  import { serverBus } from '../../main';
+
+  export default {
+    props: ['server'],
+    methods: {
+      serverSelected () {
+          serverBus.$emit('serverSelected', this.server);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
