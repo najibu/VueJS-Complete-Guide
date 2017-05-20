2a1d7eb2a198a148b8516951291d751b7c7e1141
najibu <ns.najibu@gmail.com>
2017-05-20 14:44:13 +0100

    Finished assignment

---
+++ 9_advanced_components/advanced_components/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <button @click="selectedComponent= 'appQuote'">Quote</button>
+              <button @click="selectedComponent= 'appAuthor'">Author</button>
+              <button @click="selectedComponent= 'appNew'">New</button>
+              <hr>
+              <keep-alive>
+                <component :is="selectedComponent"></component>
+              </keep-alive>
+               <!-- <appQuote>
+                 <h2 slot="title">{{ quoteTitle }}</h2>
+                 <p>A wonderful quote</p>
+               </appQuote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+  import Author from './components/Author.vue';
+  import New from './components/New.vue';
+
+    export default {
+        data () {
+          return {
+            quoteTitle: 'The Qoute',
+            selectedComponent: 'appQuote'
+          }
+        },
+        components: {
+          appQuote: Quote,
+          appAuthor: Author,
+          appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 9_advanced_components/advanced_components/src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h3>New Quote</h3>
+      <button @click="counter++">Increase!</button>
+      <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data () {
+          return {
+            counter: 0
+          }
+        },
+        destroyed () {
+          console.log('destroyed!');
+        },
+        deactivated () {
+          console.log('deactivated!');
+        },
+        activated () {
+          console.log('activated!');
+        }
+    }
+</script>
+
+
