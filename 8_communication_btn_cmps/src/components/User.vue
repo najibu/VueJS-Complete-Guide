69e39f607bdb953527ad169ece5fee0132e5a18c
najibu <ns.najibu@gmail.com>
2017-05-20 11:29:29 +0100

    Added components communication

---
+++ 8_communication_btn_cmps/src/components/User.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is: {{ name }}</p>
+        <p>Age is: {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    :myName="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"
+                ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                    :userAge="age" 
+                    @ageWasEdited="age = $event"
+                ></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                name: 'Najibu',
+                age: 27
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'Nsubuga'
+            },
+            resetName() {
+                this.name = 'Najibu';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
