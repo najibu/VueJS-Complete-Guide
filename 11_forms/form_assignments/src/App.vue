fc5c1c0915b180cd0abc42edc5eca48a10e520e4
najibu <ns.najibu@gmail.com>
2017-05-28 16:40:44 +0100

    Completed form assignment

---
+++ 11_forms/form_assignments/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row" v-if="!submitted">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label>Mail</label>
+                        <input type="email" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                    <button type="submit" @click.prevent="submitted = true" class="btn btn-primary">Submit the Form</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="submitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }} </p>
+                        <p>Mail: {{ email }} </p>
+                        <p>Password: {{ password }} </p>
+                        <p>Store in Database?: {{ storeData }} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './FullName.vue';
+
+    export default {
+       data () {
+        return {
+            fullName: 'Najibu Nsubuga',
+            email: '',
+            password: '',
+            storeData: 'Yes',
+            submitted: false
+        }
+       },
+       components: {
+            appFullName: FullName
+        } 
+    }
+</script>
+
+<style>
+</style>
---
+++ 11_forms/form_assignments/src/FullName.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label>First Name</label>
+      <input 
+      type="text" 
+      class="form-control" 
+      :value="firstName"
+      @input="nameChanged(true, $event)">
+    </div>
+    <div class="form-group">
+      <label>Last Name</label>
+      <input 
+      type="text" 
+      class="form-control" 
+      :value="lastName"
+      @input="nameChanged(false, $event)">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    methods: {
+      nameChanged (isFirst, event) {
+        let name = '';
+        if (isFirst) {
+          name = event.target.value + ' ' + this.lastName;
+        } else {
+          name = this.firstName + ' ' + event.target.value;
+        }
+        this.value = name;
+        this.$emit('input', this.value);
+      }
+    },
+    computed: {
+      firstName () {
+        return this.value.split(" ")[0];
+      },
+      lastName () {
+        return this.value.split(" ")[1];
+      }
+    }
+  }
+</script>
