3cc31fc76b086c0a68f1adf33aebd7e770527bd7
najibu <ns.najibu@gmail.com>
2017-05-29 18:10:22 +0100

    Added directives

---
+++ 12_directives/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+                <p v-text="'Some text'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight:background.delayed="'red'">Color this</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>
